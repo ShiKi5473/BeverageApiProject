8c4fff60e58d9cd4fe66c36af5edeb15401d060a
ShiKi <qz5473@gmail.com>
2025-07-31 13:42:42 +0800

    Initial commit

---
+++ src/main/java/tw/niels/beverage_api_project/BeverageApiProjectApplication.java
@@ -0,0 +1,13 @@
+package tw.niels.beverage_api_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeverageApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeverageApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tw/niels/beverage_api_project/BeverageApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.niels.beverage_api_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeverageApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
