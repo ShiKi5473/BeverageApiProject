e021458a09299c526b364f583b5021da5b8d5b80
ShiKi <qz5473@gmail.com>
2025-07-31 23:23:22 +0800

    0731 23:23 create brand entity

---
+++ src/main/java/tw/niels/beverage_api_project/modules/brand/entity/Brand.java
@@ -0,0 +1,46 @@
+package tw.niels.beverage_api_project.modules.brand.entity;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import tw.niels.beverage_api_project.modules.brand.enums.BrandStatus;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "brands")
+public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "brand_id")
+    private Integer brandId;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "name", length = 100)
+    private String contactPerson;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="status", nullable=false)
+    private BrandStatus status;
+
+    @CreatedDate
+    @Column(name="created_at", nullable=false, updatable=false)
+    private Instant createAt;
+
+}
---
+++ src/main/java/tw/niels/beverage_api_project/modules/brand/enums/BrandStatus.java
@@ -0,0 +1,6 @@
+package tw.niels.beverage_api_project.modules.brand.enums;
+
+public enum BrandStatus {
+    ACTIVE,
+    INACTIVE
+}
