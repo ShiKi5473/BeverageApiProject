b39c0d20ec41c0e9ccdb7b80c578ac85405d8af6
ShiKi <qz5473@gmail.com>
2025-12-27 01:51:26 +0800

    refactor(kds): remove unused @EventListener annotation in KdsService

--- src/main/java/tw/niels/beverage_api_project/modules/kds/service/KdsService.java
+++ src/main/java/tw/niels/beverage_api_project/modules/kds/service/KdsService.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.event.TransactionPhase;
